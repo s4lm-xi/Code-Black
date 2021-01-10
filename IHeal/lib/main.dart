d254866d7b5caf0c70fc444c8be3f45a2190eb51
qNOVAq <s4lmsheikh@gmail.com>
2021-01-10 11:49:39 +0300

    Modified

--- IHeal/lib/main.dart
+++ IHeal/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -23,11 +24,59 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: Center(child: Text('My Page!')),
+
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(5),
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue, width: 5.0),
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                child: Row(
+                  children: [
+
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: NetworkImage('https://i.pinimg.com/originals/83/72/e9/8372e957fc617e9e956f116afd3e599b.jpg'),
+
+                        ),
+                      ),
+                    ),
+
+                    Text('''
+            Name: Brad Pit
+            
+            Age: 57
+            
+            Height: 1.8 m
+            
+            ''', style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                    ),),
+
+                  ],
+                ),
+
+              ),
+            ],
+          ),
+
+        ],
+      ),
+
+
+
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
         child: ListView(
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
@@ -35,63 +84,134 @@ class MyHomePage extends StatelessWidget {
             DrawerHeader(
 
               child: FittedBox(
-                child: Image.asset('assets/logo.png'),
-                fit: BoxFit.fill,
+                child: Image.asset('assets/logo.jpg'),
+                fit: BoxFit.fitWidth,
               )
 
 
             ),
-            ListTile(
-              title: Text('Ask A Doctor', style: TextStyle(
-                fontSize: 25.0
+            Container(
+               margin: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(5.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue, width: 5.0)
+              ),
+              child: ListTile(
+                title: Text('Ask A Doctor', style: TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
 
-              ),),
-              onTap: () {
-                Navigator.push(context,
-                MaterialPageRoute(builder: (context) => AskDoctor()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Find A Pharmacy'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-              ListTile(
-                title: Text('Medical Results'),
+
+                ),),
                 onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);}
-                ),
-            ListTile(
-                title: Text('Appointments'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);}
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AskDoctor()),
+                  );
+                },
+              ),
             ),
-            ListTile(
-                title: Text('Laboratory Tests'),
+
+            Container(
+              margin: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 5.0)
+              ),
+              child: ListTile(
+                title: Text('Find A Pharmacy', style: TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
+
+
+                ),),
                 onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);}
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => FindPharmacy()),
+                  );
+                },
+              ),
             ),
-            ListTile(
-                title: Text('Reviews'),
+
+            Container(
+              margin: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 5.0)
+              ),
+              child: ListTile(
+                title: Text('Medical Results', style: TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
+
+
+                ),),
                 onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);}
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MedicalResults()),
+                  );
+                },
+              ),
+            ),
+
+            Container(
+              margin: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 5.0)
+              ),
+              child: ListTile(
+                title: Text('Appointments', style: TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
+
+
+                ),),
+                onTap: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AskDoctor()),
+                  );
+                },
+              ),
+            ),
+
+            Container(
+              margin: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 5.0)
+              ),
+              child: ListTile(
+                title: Text('Laboratory Tests', style: TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
+
+
+                ),),
+                onTap: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AskDoctor()),
+                  );
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 5.0)
+              ),
+              child: ListTile(
+                title: Text('Reviews', style: TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
+
+                ),),
+                onTap: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AskDoctor()),
+                  );
+                },
+              ),
             ),
           ],
         ),
@@ -100,20 +220,80 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+
+
 class AskDoctor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TTIITITle'),
+        title: Text('Ask A Doctor'),
       ),
-      body: Center(
-
-        child: ElevatedButton(
-          onPressed: (){},
-        ),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 2,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(8, (index) {
+          return Center(
+            child: Text(
+              'Doctor $index',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          );
+        }),
       ),
-
     );
   }
 }
+
+
+
+class FindPharmacy extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask A Doctor'),
+      ),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 2,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(8, (index) {
+          return Center(
+            child: Text(
+              'Pharmacy $index',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+
+class MedicalResults extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Medical Results'),
+      ),
+
+      body: Container(
+        child: Row(
+          children: [
+            Image.asset('assets/graph.jpg',
+             width: 195, height: 550,),
+            Image.asset('assets/graph1.jpg', width: 195, height: 550,),
+
+          ],
+        ),
+      )
+    );
+  }
+}
+
